61fd73fbfc8cfc0ea28526e28de0e3729a614081
Mykola Volokyta <nikv2005@gmail.com>
2024-04-16 11:20:56 +0300

    Init commit

---
+++ PaymentAndTokenManagement/Program.cs
@@ -0,0 +1,34 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ConsumerConfig
+{
+    GroupId = "tournament-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+consumer.Subscribe("tournament-topic");
+
+CancellationTokenSource token = new();
+
+try
+{
+    while (true)
+    {
+        var responce = consumer.Consume(token.Token);
+        if (responce.Message != null)
+        {
+            var tournament = JsonConvert.DeserializeObject<Tournament>(responce.Message.Value);
+            Console.WriteLine($"Name: {tournament.Name}, Price: {tournament.Price}");
+        }
+    }
+}
+catch (Exception)
+{
+    throw;
+}
+
+public record Tournament(string Name, int Price);
---
+++ TournamentManagementService/Program.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    string? name = "Bebra";
+    int? price = 228;
+    while (true) {
+        Console.Write("Name of Tournament: ");
+        name = Console.ReadLine();
+        Console.Write("Price to enter: ");
+        price = Convert.ToInt32(Console.ReadLine());
+        if (name == null || price == null)
+        {
+            break;
+        }
+        var responce = await producer.ProduceAsync("tournament-topic", new Message<Null, string> { Value = JsonConvert.SerializeObject(new Tournament(name, price.Value))});
+        Console.WriteLine(responce.Value);
+    }
+}
+catch (ProduceException<Null, string> ex)
+{
+    Console.WriteLine(ex.Message);
+}
+public record Tournament(string Name, int Price);
